5c70fc1bcf9797eff2fa9c9857a4ace53b471043
Erick Villeda <eeverickeev@gmail.com>
2020-09-08 02:08:52 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,38 @@
 # Write your code here!
+require 'pry'
+def game_hash
+  hashketball_info = Hash.new
+  stats = Array.new
+  hashketball_info = {
+    :home => {
+      :team_name => {"Brooklyn Nets"},
+      :colors => {"Black, White"},
+      :players => {
+        :player_name => {"Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"},
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
+      }
+    },
+    :away => {
+      :team_name => {},
+      :colors => {},
+      :players => {
+        :player_name => {},
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
+      }
+    }
+  }
+end
